6e1e1b888c2a78dc348d957d574229ddeaa2ca27
Teresia Olsson <teresia.olsson@diamond.ac.uk>
2021-12-03 15:17:39 +0000

    Change name of longitudinal output file.

--- generate_wake_impedance/save_elegant_impedance_file.m
+++ generate_wake_impedance/save_elegant_impedance_file.m
@@ -6,7 +6,7 @@ function save_elegant_impedance_file(elegant_impedance)
         fprintf(fileID,'%10e %10e\n',elegant_impedance.ImpedanceFreq(i),elegant_impedance.ImpedanceRealZ(i));
     end
     fclose(fileID);
-    system('plaindata2sdds elegant_real_lon_impedance.txt impedance_real_elegant.sdds -inputMode=ascii -outputMode=ascii -separator=" " -noRowCount -column=Freq,double,units=Hz  -column=RealZ,double,units=Ohm');
+    system('plaindata2sdds elegant_real_lon_impedance.txt impedance_longitudinal_real_elegant.sdds -inputMode=ascii -outputMode=ascii -separator=" " -noRowCount -column=Freq,double,units=Hz  -column=RealZ,double,units=Ohm');
     delete('elegant_real_lon_impedance.txt');
 
     fileID = fopen('elegant_imag_lon_impedance.txt','w');
@@ -14,7 +14,7 @@ function save_elegant_impedance_file(elegant_impedance)
         fprintf(fileID,'%10e %10e\n',elegant_impedance.ImpedanceFreq(i),elegant_impedance.ImpedanceImagZ(i));
     end
     fclose(fileID);
-    system('plaindata2sdds elegant_imag_lon_impedance.txt impedance_imag_elegant.sdds -inputMode=ascii -outputMode=ascii -separator=" " -noRowCount -column=Freq,double,units=Hz  -column=ImagZ,double,units=Ohm');
+    system('plaindata2sdds elegant_imag_lon_impedance.txt impedance_longitudinal_imag_elegant.sdds -inputMode=ascii -outputMode=ascii -separator=" " -noRowCount -column=Freq,double,units=Hz  -column=ImagZ,double,units=Ohm');
     delete('elegant_imag_lon_impedance.txt');
 
     fileID = fopen('elegant_transverse_impedance.txt','w');
